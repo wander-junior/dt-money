53a04849787bc73f5379ea3a8522c1ec7b828b37
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-07-18 19:14:50 -0300

    Move creation of transaction to context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import { useState, FormEvent, useContext } from 'react';
 import Modal from 'react-modal';
+import { TransactionsContext } from '../../TransactionsContext';
+import { api } from '../../services/app';
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-
-import { api } from '../../services/app';
 
 import { Container, TransactionTypeContainer, RadioBox } from './style';
 
@@ -17,22 +18,22 @@ interface NewTransactionModalProps {
 Modal.setAppElement('#root');
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const { createTransaction } = useContext(TransactionsContext)
+
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0)
+    const [amount, setAmount] = useState(0)
     const [category, setCategory] = useState('')
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: React.FormEvent) {
+    function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {
+        createTransaction({
             title, 
-            value, 
-            category, 
-            type
-        }
-
-        api.post('/transactions', data)
+            amount,
+            type,
+            category,
+        })
 
         onRequestClose();
     }
@@ -64,8 +65,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input
                     type="number"
                     placeholder="Valor"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,7 +3,7 @@ import { Container } from "./style";
 import { TransactionsContext } from '../../TransactionsContext';
 
 export function TransactionTable() {
-    const transactions = useContext(TransactionsContext)
+    const { transactions } = useContext(TransactionsContext)
 
     return (
         <Container>
